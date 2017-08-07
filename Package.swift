2aa6573ada424bf2b46e19d30efe4ad7d549cb6d
dndydon <don@sleeter.com>
2017-08-07 03:06:32 -0700

    Commander and Spectre Swift 4 conversions

--- Package.swift
+++ Package.swift
@@ -1,8 +1,26 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "Commander",
-  dependencies: [
-    .Package(url: "https://github.com/kylef/Spectre.git", majorVersion: 0, minor: 7),
-  ]
+    name: "Commander",
+    products: [
+        .library(
+            name: "Commander",
+            targets: ["Commander"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/kylef/Spectre.git", from: "0.7.1"),
+    ],
+    targets: [
+        .target(
+            name: "Commander",
+            dependencies: ["Spectre"],
+            path: "./Sources"),
+        .testTarget(
+            name: "CommanderTests",
+            dependencies: ["Spectre"],
+            path: "./Tests"),
+    ]
 )
